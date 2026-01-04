a3c1b0400a93abee6ce3aefa378a8fde9a3f1e7b
Nazar Bodan <nazar.bodan@gmail.com>
2026-01-04 12:56:06 +0200

    feat: Add web component lifecycle callback demonstration with a custom-square element and interactive controls.

---
+++ life-cycle-callbacks/main.js
@@ -0,0 +1,91 @@
+class Square extends HTMLElement {
+	// Specify observed attributes so that attributeChangedCallback will work
+	static get observedAttributes() {
+		return ["size", "color"];
+	}
+
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({ mode: "open" });
+
+		const div = document.createElement("div");
+		const style = document.createElement("style");
+
+		shadow.appendChild(div);
+		shadow.appendChild(style);
+	}
+
+	connectedCallback() {
+		console.log("Custom square element added to page.");
+		updateStyle(this);
+	}
+
+	disconnectedCallback() {
+		console.log("Custom square element removed from page.");
+	}
+
+	adoptedCallback() {
+		console.log("Custom square element moved to new page");
+	}
+
+	attributeChangedCallback() {
+		console.log("Custom square element attributes changed");
+		updateStyle(this);
+	}
+}
+
+customElements.define("custom-square", Square);
+
+function updateStyle(element) {
+	const shadow = element.shadowRoot;
+	shadow.querySelector("style").textContent = `
+    div {
+      width: ${element.getAttribute("size")}px;
+      height: ${element.getAttribute("size")}px;
+      background-color: ${element.getAttribute("color")};
+    }
+  `;
+}
+
+const add = document.querySelector(".add");
+const update = document.querySelector(".update");
+const remove = document.querySelector(".remove");
+let square;
+
+update.disabled = true;
+remove.disabled = true;
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+add.onclick = () => {
+	// Create a custom square element
+	square = document.createElement("custom-square");
+	square.setAttribute("size", 100);
+	square.setAttribute("color", "red");
+	document.body.appendChild(square);
+
+	update.disabled = false;
+	remove.disabled = false;
+	add.disabled = true;
+};
+
+update.onclick = () => {
+	// Randomly update square's attributes
+	square.setAttribute("size", random(50, 200));
+	square.setAttribute(
+		"color",
+		`rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`,
+	);
+};
+
+remove.onclick = () => {
+	// Remove the square
+	document.body.removeChild(square);
+
+	update.disabled = true;
+	remove.disabled = true;
+	add.disabled = false;
+};
