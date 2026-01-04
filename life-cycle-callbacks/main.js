d04cb77d6f3d6518962400f0590da8b072db10c2
Nazar Bodan <nazar.bodan@gmail.com>
2026-01-04 13:33:21 +0200

    refactor: Replace `onclick` assignments with `addEventListener` for event handling.

--- life-cycle-callbacks/main.js
+++ life-cycle-callbacks/main.js
@@ -12,7 +12,7 @@ function random(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-add.onclick = () => {
+add.addEventListener("click", () => {
 	// Create a custom square element
 	square = document.createElement("custom-square");
 	square.setAttribute("size", 100);
@@ -22,22 +22,22 @@ add.onclick = () => {
 	update.disabled = false;
 	remove.disabled = false;
 	add.disabled = true;
-};
+});
 
-update.onclick = () => {
+update.addEventListener("click", () => {
 	// Randomly update square's attributes
 	square.setAttribute("size", random(50, 200));
 	square.setAttribute(
 		"color",
 		`rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`,
 	);
-};
+});
 
-remove.onclick = () => {
+remove.addEventListener("click", () => {
 	// Remove the square
 	document.body.removeChild(square);
 
 	update.disabled = true;
 	remove.disabled = true;
 	add.disabled = false;
-};
+});
